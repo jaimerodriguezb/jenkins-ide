bcf17a83578ffa575ae3f26c867714c7d87b65bc
Jaime Rodriguez <jaimerodriguez@gmail.com>
2023-06-10 17:26:46 -0500

    post action email notification

--- pipeline.groovy
+++ pipeline.groovy
@@ -17,18 +17,28 @@ pipeline {
                 // Get some code from a GitHub repository
                 git branch: 'main', url: 'https://github.com/g0t4/jgsu-spring-petclinic.git'
                 
-                bat '.\\mvnw compile'
+                //bat '.\\mvnw compile'
                 
-                bat '.\\mvnw package'
+                //bat '.\\mvnw package'
+
+                bat 'false'
 
                 // junit 'target/surefire-reports/*.xml'
 
             }
 
             post {
-                always {
-                    junit '*target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
+                //always {
+                //    junit '*target/surefire-reports/TEST-*.xml'
+                //    archiveArtifacts 'target/*.jar'
+                //}
+                changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build.",
+                        attachLog: true,  
+                        compressLog: true, 
+                        to: 'dev@uan.edu.co',
+                        recipientProviders: [requestor(), upstreamDevelopers()]
                 }
             }
         }
